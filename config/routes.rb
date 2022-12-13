7c44ba988a4785c8e9d3813faf8cefd11ec30e54
John DeSilva <desilvjo@umich.edu>
2022-12-13 16:05:49 -0500

    Update README.md

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'books#new'
+
   resources :books
   resources :authors
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
